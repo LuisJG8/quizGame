66f51a5a19707a5971a9fca7d2a85a9683005e9b
Luis Gonzalez <162064187+LuisJG8@users.noreply.github.com>
2024-07-25 19:16:23 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,32 @@
+from data import question_data
+from question_model import Question
+from quiz_brain import QuizBrain
+
+question_bank = []
+for thing in question_data:
+    question_text = thing["question"]
+    question_answer = thing["correct_answer"]
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
+
+quiz = QuizBrain(question_bank)
+
+while quiz.still_has_questions():
+    quiz.next_question()
+
+print("You completed the quiz!")
+print(f"Your final score is: {quiz.count}/{quiz.question_number}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ quiz_brain.py
@@ -0,0 +1,28 @@
+class QuizBrain:
+
+    def __init__(self, q_list):
+        self.question_number = 0
+        self.count = 0
+        self.question_list = q_list
+
+
+    def still_has_questions(self):
+        return self.question_number < len(self.question_list)
+
+
+    def next_question(self):
+        current_question = self.question_list[self.question_number]
+        self.question_number += 1
+        user_answer = input(f"Q.{self.question_number}: {current_question.text} (True or False): ")
+        self.check_answer(user_answer, current_question.answer)
+
+    def check_answer(self, user_answer, correct_answer):
+        if user_answer.lower() == correct_answer.lower():
+            self.count += 1
+            print("Correct answer")
+            print(f"You current score is {self.count}/{self.question_number}")
+        else:
+            print("Incorrect answer")
+            print(f"The correct answer is {correct_answer}")
+            print(f"You current score is {self.count}/{self.question_number}")
+        return self.count, self.question_number
